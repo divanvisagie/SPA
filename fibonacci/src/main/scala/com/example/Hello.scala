d6ec188d01135929b5a1f083db1ce038708cdb56
Divan Visagie <visagiedivan@gmail.com>
2016-03-23 22:25:58 +0200

    naming cleanup

--- fibonacci/src/main/scala/com/example/Hello.scala
+++ fibonacci/src/main/scala/com/example/Hello.scala
@@ -10,27 +10,26 @@ case class OutMessage(val answer: Int)
 
 class Handler extends IRPCHandler {
   implicit val formats = Serialization.formats(NoTypeHints)
-  def tailRecursiveBig(n :Int) : Int = {
+  def fibonacci(n :Int) : Int = {
     @tailrec
-    def aux(n : Int, next :Int, acc :Int) :Int ={
+    def fib(n : Int, next :Int, acc :Int) :Int = {
       if(n == 0) acc
-      else aux(n-1, acc + next,next)
+      else fib(n-1, acc + next,next)
     }
-    aux(n,1,0)
+    fib(n,1,0)
   }
 
   def handleMessage(message: String): String = {
     val inMessage = read[InMessage](message)
-    val answer = tailRecursiveBig(inMessage.number)
+    val answer = fibonacci(inMessage.number)
     write(OutMessage(answer))
   }
 }
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-
-    var rpcServer = new RPCServer("fibonacci",new Handler())
+    val rpcServer = new RPCServer("fibonacci",new Handler())
+    println("Start listening")
     rpcServer.listen()
   }
 }
